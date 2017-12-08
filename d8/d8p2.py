4d4473c17184f3222956098a63d7332b3f11f868
Javier Abadía <javi.abadia@gmail.com>
2017-12-08 08:20:01 +0100

    arreglo un poco el código

--- d8/d8p2.py
+++ d8/d8p2.py
@@ -1010,45 +1010,46 @@ wby inc -397 if ntk >= -2941
 nm dec 907 if qen == 2233
 """
 
+operators = {
+    '<': lambda a, b: a < b,
+    '>': lambda a, b: a > b,
+    '==': lambda a, b: a == b,
+    '!=': lambda a, b: a != b,
+    '<=': lambda a, b: a <= b,
+    '>=': lambda a, b: a >= b,
+    'inc': lambda a, b: a + b,
+    'dec': lambda a, b: a - b,
+}
+
 
 def execute(program):
     registers = {}
     max_value = 0
     for line in program.strip().split('\n'):
         print(line)
-        # groups = re.match(r'^([a-z]+) inc|dec (-?\d+) if ([a-z]+) (<|>|==|!=|<=|>=) (-?\d+)', line.strip())
-        groups = re.match(r'^([a-z]+) ((inc)|(dec)) (-?\d+) if ([a-z]+) (<|>|(==)|(!=)|(<=)|(>=)) (-?\d+)',
-                          line.strip())
+        groups = re.match(
+            r'^([a-z]+) ((inc)|(dec)) (-?\d+) if ([a-z]+) (<|>|(==)|(!=)|(<=)|(>=)) (-?\d+)',
+            line.strip()
+        )
         assert groups, 'no match %s' % (line,)
-        register, operator, operand, cond_register, cond_operator, cond_operand = \
-            groups[1], \
-            groups[2], \
-            int(groups[5]), \
-            groups[6], \
-            groups[7], \
-            int(groups[12])
+        register, operator, operand = groups[1], groups[2], int(groups[5])
+        cond_register, cond_operator, cond_operand = groups[6], groups[7], int(groups[12])
 
         print(register, operator, operand, cond_register, cond_operator, cond_operand)
 
-        operators = {
-            '<': lambda a, b: a < b,
-            '>': lambda a, b: a > b,
-            '==': lambda a, b: a == b,
-            '!=': lambda a, b: a != b,
-            '<=': lambda a, b: a <= b,
-            '>=': lambda a, b: a >= b,
-            'inc': lambda a, b: a+b,
-            'dec': lambda a, b: a-b,
-        }
-        cond_value = registers.get(cond_register, 0)
-        condition = operators[cond_operator](cond_value, cond_operand)
+        condition = operators[cond_operator](
+            registers.get(cond_register, 0),
+            cond_operand
+        )
 
         if condition:
-            value = registers.get(register, 0)
-            registers[register] = operators[operator](value, operand)
-            if registers[register] > max_value:
-                max_value = registers[register]
+            registers[register] = operators[operator](
+                registers.get(register, 0),
+                operand
+            )
+            max_value = max(registers[register], max_value)
 
     print("max:", max(registers.values()), max_value)
+
 
 execute(PROGRAM)
